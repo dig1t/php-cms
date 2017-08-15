18da00185f96d23df5fd8fe0167074901e82d192
Javier Mejia <4343867+dig1t@users.noreply.github.com>
2017-08-14 19:14:33 -0500

    v0.1.3.1

--- tests/seo-test.php
+++ tests/seo-test.php
@@ -4,7 +4,7 @@ require_once('lib/SEO.php');
 $seo = new SEOBuild();
 
 $seo->add(array(
-	'title' => 'cat website',
+	'title' => 'Personal Website',
 	'description' => 'A short description.',
 	
 	// automatically added to opengraph & twitter tags
@@ -14,8 +14,8 @@ $seo->add(array(
 
 $seo->addTwitter(array(
 	'card' => 'summary', // type of twitter card
-	'site' => '@cats', // website profile
-	'creator' => '@digitalscape_', // author profile
+	'site' => '@Digitalscape_', // website profile
+	'creator' => '@Digitalscape_', // author profile
 ));
 
 // echo out all given tags
